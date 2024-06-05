476a2d16eb306e6ae57c069d55a314f4079b790d
taf-s <tafara.sithole@gmail.com>
2024-06-05 16:46:44 +0200

    applied the Singeleton Pattern to the Bank branch management

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
+
+// 1. Variable to store the singleton instance of the bank branch. "bankBranchInstance"
+
+let bankBranchInstance = null;
+
+// 2. Class called `BankBranch` for managing branch information.
+class BankBranch{
+
+// 3. Constructor in the `BankBranch` class
+
+    constructor (branchinfo){
+        if(!bankBranchInstance){
+            bankBranchInstance = this;
+            this.branchinfo = branchinfo;
+
+        }
+        return bankBranchInstance;
+    }
+
+ // 4. Method to the `BankBranch` class to retrieve branch details.
+    getBranchInfo() {
+        return this.branchinfo;
+    }
+}
+
+// 5. In the usage section:
+
+const branchA = new BankBranch ('Branch A');
+const branchB = new BankBranch ('Branch B');
+
+console.log (branchA === branchB);
+console.log (branchA.getBranchInfo());
+console.log (branchB.getBranchInfo());
+
+
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
 
 // 2. Define a class called `BankBranch` for managing branch information.
@@ -32,3 +67,4 @@
 // - The `getBranchInfo` method allows us to retrieve the branch information from the singleton instance.
 // - We demonstrate the Singleton pattern by creating two instances (`branchA` and `branchB`) and show that they both point to the same instance,
 //   ensuring that there is only one set of branch information throughout the application.
+
